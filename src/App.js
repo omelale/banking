37ef26ea913b11ea334a8f6bb85a937db9fa15f7
Ajten <ajtencani@gmail.com>
2022-01-07 22:42:40 +0100

    display state to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import {useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div className="App App-header">
-      <h1>Ome</h1>
-    </div>
-  );
+    const account = useSelector((state)=>{return state.account})
+    console.log(account)
+    return (<div className="App App-header">
+        <h1>Ome</h1>
+    </div>);
 }
 
 export default App;
