ee74317fc8eea84fe1402ed17d708ffc9783d5f4
Ajten <ajtencani@gmail.com>
2022-01-08 18:07:31 +0100

    added 2 action creators

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -1,3 +1,17 @@
-export const DepositMoney = (amount) => {
+export const depositMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type : 'deposit',
+            amount : amount
+        })
+    }
+}
 
+export const withdrawMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type : 'withdraw',
+            amount: amount
+        })
+    }
 }
